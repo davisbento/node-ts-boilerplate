211b800471bb39e64c5a520b5312ed0714ed9084
davisbento <davisilva4222@gmail.com>
2019-09-17 17:58:28 -0300

    Updated packages

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -3,7 +3,6 @@ import { NextFunction, Request, Response } from 'express';
 class HomeController {
   public async index(req: Request, res: Response, next: NextFunction) {
     try {
-      throw new Error('error aqui');
       res.json({
         message: 'home',
       });
